7ffe423c64c03000e71941878523110185429128
Ayush92003 <ak8419957@gmail.com>
2025-11-17 23:11:40 +0530

    db and tools setup

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,9 +1,9 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ quiet: true });
 
 export const ENV = {
-    PORT : process.env.PORT,
-    db_url : process.env.db_url,
-    NODE_ENV:process.env.NODE_ENV
-}
+  PORT: process.env.PORT,
+  db_url: process.env.db_url,
+  NODE_ENV: process.env.NODE_ENV,
+};
